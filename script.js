00d8ce37f165b4935921e700706038cd087de63d
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-03-01 09:34:43 -0600

    Completed exercise

---
+++ script.js
@@ -0,0 +1,25 @@
+for (i = 1; i <= 7; i ++){
+    console.log(i);
+}
+
+console.log(`=======================`);
+
+for (i =5; i <= 25; i = i + 4){
+    console.log(i);
+}
+
+console.log(`=======================`);
+
+const wizards = [`Harry Potter`, `Hermione Granger`, `Ron Weasly`]
+for (wizard of wizards){
+    console.log(wizard);
+}
+
+console.log(`=======================`);
+
+let harryPotterMovies = 0
+
+while  (harryPotterMovies != 8){
+harryPotterMovies = harryPotterMovies + 1
+console.log(harryPotterMovies);
+}
