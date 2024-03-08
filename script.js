db17d9deb5a8cf5900ad0daee4c8979203e2933b
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-03-08 08:05:55 -0600

    Completed bonus

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ while  (harryPotterMovies != 8){
 harryPotterMovies = harryPotterMovies + 1
 console.log(harryPotterMovies);
 }
+
+const hogwartsHouses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+for (houses of hogwartsHouses){
+  for (char of houses){
+    console.log(char);
+  }
+}
+
+const quote = ["Yer", "A", "Wizard", "Harry"]
+let quote2 = ""
+for(char of quote) {
+  quote2 += char + " "
+}
+console.log(quote2)
+
+for(i=1; i<=25; i++) {
+    if (i % 3 == 0 && i % 5 ==0) {
+        console.log("Expecto Patronum")
+    } else 
+      if (i % 3 == 0) {
+        console.log("Expecto")
+    } else
+       if (i % 5 == 0) {
+        console.log("Patronum")
+    } else {
+        console.log(i)
+    }
+}
